578df2cc064e7c13e6ca223566ff14cf5aee3997
Agostina Rubbini <168373685+agosrubbini@users.noreply.github.com>
2024-11-16 00:58:14 -0300

    Add files via upload

---
+++ Practica/Ejercicio1/Tweet.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.oo2.Ejercicio1;
+
+public class Tweet {
+
+	private String texto;
+	
+	
+	public Tweet (String tx) {
+		this.texto=tx;
+	}
+	
+	//devuelve el texto, entonces si el rt tiene q dovolver el texto se lo da el tw, en una interfas
+	//Un metodo que devuelva si esta relacionado con el usuario q recibe 
+	//eliminar usr: 
+	
+}
